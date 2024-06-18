ff7a69aa051d98592cde97962fd2f8460a0df00f
Rajashekar <raja.kayyam1@gmail.com>
2024-06-18 17:02:00 -0600

    Linkedlist

---
+++ src/LinkedList/LinkedListCreation.java
@@ -0,0 +1,65 @@
+package LinkedList;
+
+public class LinkedListCreation {
+	
+	private Node head;
+	private Node tail;
+	private int length;
+	
+	
+	public LinkedListCreation( int value ){
+		Node newnode = new Node(value);
+		head =newnode ;
+		tail=newnode;
+		
+		
+	}
+	
+	class Node {
+		
+		int value;
+		Node next;
+		Node (int value){
+			this.value=value;
+		}
+		
+		
+		}
+	public void printlist() {
+		Node temp=head;
+		while(temp !=null) {
+			System.out.println(temp.value);
+			temp=temp.next;
+		}
+		
+		
+		
+	}
+	public Node getHead() {
+		return head;
+	}
+	public void setHead(Node head) {
+		this.head = head;
+	}
+	public Node getTail() {
+		return tail;
+	}
+	public void setTail(Node tail) {
+		this.tail = tail;
+	}
+	public void append(int value) {
+		Node n= new Node(value);
+		
+		if (length==0) {
+			head=n;
+			tail=n;
+		}
+		else {
+			tail.next=n;
+			tail=n;
+		}
+		length++;
+	}
+	
+
+}
---
+++ src/LinkedList/Main.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 LinkedListCreation L= new  LinkedListCreation(4);
+		
+//		 L.getTail();
+//		 L.getHead();
+		 L.append(10);
+		 L.append(3);
+		 L.append(70);
+		
+		 L.printlist();
+		
+
+	}
+
+}
